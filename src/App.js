5c9bc155f57fa6a27dc29aa7c8e814f173f4e285
James Grieve <pocojames@hotmail.com>
2021-01-27 17:20:37 -0800

    add first input component for general info

--- src/App.js
+++ src/App.js
@@ -13,23 +13,61 @@ class App extends Component {
       generalInfo: [],
       experienceInfo: [],
       educationalInfo: [],
+      generalInfoForm: true,
+      experienceInfoForm: true,
+      educationalInfoForm: true,
     };
   }
 
-  // click submit then u iterate through all inputs, colecting the values,
-  // then u
-  // update this app with the values. then u run another function here
-  // that will then  go thrpughh each dom element and make it a p
-  // also renders a edit btn
-
-  // submitGeneralInfo = (e) => {
-  //   e.preventDefault();
-  //   this.setState = {
-  //     generalInfo:
-  //     experienceInfo: this.state.experienceInfo,
-  //     educationalInfo: this.state.educationalInfo
-  //   }
-  // };
+  submitFormData = (e) => {
+    e.preventDefault();
+    const formInputs = document.getElementById(e.target.id);
+    let infoArray = [];
+    for (let i = 0; i < formInputs.length; i++) {
+      infoArray.push(formInputs[i].value);
+    }
+    infoArray.pop();
+    if (
+      infoArray.length !== formInputs.length - 1 ||
+      infoArray.some((el) => el.trim() === '')
+    ) {
+      alert('Please complete each input.');
+      return;
+    }
+    this.setState(function (state) {
+      switch (e.target.id) {
+        case 'general-info-form':
+          return {
+            generalInfo: infoArray,
+            experienceInfo: state.experienceInfo,
+            educationalInfo: state.educationalInfo,
+            generalInfoForm: false,
+            experienceInfoForm: state.experienceInfoForm,
+            educationalInfoForm: state.educationalInfoForm,
+          };
+        case 'educational-info-form':
+          return {
+            generalInfo: state.generalInfo,
+            experienceInfo: infoArray,
+            educationalInfo: state.educationalInfo,
+            generalInfoForm: state.generalInfoForm,
+            experienceInfoForm: state.experienceInfoForm,
+            educationalInfoForm: false,
+          };
+        case 'experience-info-form':
+          return {
+            generalInfo: state.generalInfo,
+            experienceInfo: state.experienceInfo,
+            educationalInfo: infoArray,
+            generalInfoForm: state.generalInfoForm,
+            experienceInfoForm: false,
+            educationalInfoForm: state.educationalInfoForm,
+          };
+        default:
+          return;
+      }
+    });
+  };
 
   render() {
     return (
@@ -37,7 +75,28 @@ class App extends Component {
         <div className="title">REACT CV App</div>
         <div className="container">
           <div className="cv-block">
-            <div className="generalInfo-block">{<GeneralInfo />}</div>
+            {this.state.generalInfoForm ? (
+              <div className="generalInfo-block">
+                <form onSubmit={this.submitFormData} id="general-info-form">
+                  <label htmlFor="first-name">First Name:</label>
+                  <input id="first-name" />
+                  <label htmlFor="last-name">Last Name:</label>
+                  <input id="last-name" />
+                  <label htmlFor="email">Email:</label>
+                  <input id="email" />
+                  <label htmlFor="phone-number">Phone Number:</label>
+                  <input id="phone-number" />
+                  <button>Submit</button>
+                </form>
+                <hr />
+              </div>
+            ) : (
+              <div>
+                <GeneralInfo formInfo={this.state.generalInfo} />
+                <button>Edit</button>
+                <hr />
+              </div>
+            )}
             <div className="educationalInfo-block">
               <EducationalInfo />
             </div>
